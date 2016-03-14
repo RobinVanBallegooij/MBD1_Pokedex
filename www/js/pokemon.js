3e6f7b38773b60abac8ee32bc185fd9da078e628
RobinVanBallegooij <rga.vanballegooij@student.avans.nl>
2016-03-14 21:22:14 +0100

    added infinite list

--- www/js/pokemon.js
+++ www/js/pokemon.js
@@ -2,25 +2,69 @@ $(document).ready(function() {
 	loadCompendium();
 });
 
+//global variables
+var POKEMON_NUMBER = 0;
+var next = '';
+
+//functions
 function loadCompendium() {
 	var listContent = '';
 
-	listContent += '<li><a href="#">1</a></li>';
-	listContent += '<li><a href="#">2</a></li>';
-	listContent += '<li><a href="#">3</a></li>';
-	listContent += '<li><a href="#">4</a></li>';
-	listContent += '<li><a href="#">5</a></li>';
-
 	$.getJSON('http://pokeapi.co/api/v2/pokemon', function(data) {
-		$.each(data, function() {
-			console.log(data);
+
+		next = data.next;
+
+		$.each(data.results, function() {
+			POKEMON_NUMBER++;
+			listContent += '<li><a href="#">#' + POKEMON_NUMBER + ' ' + this.name + '</a></li>';
 		});
+
+		$('#compendiumListView').html(listContent);
 	});
-
-	$('#compendiumListView').html(listContent);
-
 };
 
+function loadNext(page) {
+	var nextListContent = '';
+
+	if (next !== '') {
+		$.getJSON(next, function(data) {
+			next = data.next;
+
+			$.each(data.results, function() {
+				POKEMON_NUMBER++;
+				nextListContent += '<li><a href="#">#' + POKEMON_NUMBER + ' ' + this.name + '</a></li>';
+			});
+
+			$("#compendiumListView", page).append(nextListContent).listview("refresh");
+		});
+	}
+};
+
+function addMore(page) {
+    $.mobile.loading("show", {
+        text: "loading more..",
+        textVisible: true,
+        theme: "a"
+    });
+    setTimeout(function () {
+        loadNext(page); 
+        $.mobile.loading("hide");
+    }, 500);
+}
+
+$(document).on("scrollstop", function (e) {
+    var activePage = $.mobile.pageContainer.pagecontainer("getActivePage"),
+        screenHeight = $.mobile.getScreenHeight(),
+        contentHeight = $(".ui-content", activePage).outerHeight(),
+        header = $(".ui-header", activePage).outerHeight() - 1,
+        scrolled = $(window).scrollTop(),
+        footer = $(".ui-footer", activePage).outerHeight() - 1,
+        scrollEnd = contentHeight - screenHeight + header + footer;
+    if (activePage[0].id == "compendium" && scrolled >= scrollEnd) {
+        console.log("adding...");
+        addMore(activePage);
+    }
+});
 
 /* NOTES
 
